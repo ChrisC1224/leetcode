df8a17d0aded4d7f9785d443f0c6048ec9b6fc31
ChrisC1224 <33515829+ChrisC1224@users.noreply.github.com>
2018-06-04 13:58:06 +0800

    Create Solution.py

---
+++ src/58.Length of Last Word/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == '':
+            return 0
+        length = 0
+        l = len(s)-1
+        while s[l] == ' ' and l >= 0:
+            l -= 1
+        while s[l] != ' ' and l >= 0:
+            length += 1
+            l -= 1
+        return length
