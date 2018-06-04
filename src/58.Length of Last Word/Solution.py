440a0316d9b70b9353936208693733ed8950f25f
ChrisC1224 <33515829+ChrisC1224@users.noreply.github.com>
2018-06-04 14:05:52 +0800

    Update Solution.py

--- src/58.Length of Last Word/Solution.py
+++ src/58.Length of Last Word/Solution.py
@@ -6,11 +6,15 @@ class Solution:
         """
         if s == '':
             return 0
-        length = 0
-        l = len(s)-1
-        while s[l] == ' ' and l >= 0:
-            l -= 1
-        while s[l] != ' ' and l >= 0:
-            length += 1
-            l -= 1
-        return length
+        # length = 0
+        # l = len(s)-1
+        # while s[l] == ' ' and l >= 0:
+        #     l -= 1
+        # while s[l] != ' ' and l >= 0:
+        #     length += 1
+        #     l -= 1
+        # return length
+        a = s.split()
+        if a == []:
+            return 0
+        return len(a[-1])
